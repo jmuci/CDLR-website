9b78c36873352043cd6ffcb0ac5089afeb2d602d
illera88 <agarciaillera@gmail.com>
2019-06-08 10:12:52 -0700

    increase time between testimonals

--- js/main.js
+++ js/main.js
@@ -7,21 +7,21 @@ $(document).ready(function(){
 	$("#testimonial-slider").owlCarousel({
 	    paginationSpeed : 500,      
 	    singleItem:true,
-	    autoPlay: 3000,
+	    autoPlay: 10000,
 	});
 
 
 
 
 	$("#clients-logo").owlCarousel({
-		autoPlay: 3000,
+		autoPlay: 10000,
 		items : 5,
 		itemsDesktop : [1199,5],
 		itemsDesktopSmall : [979,5],
 	});
 
 	$("#works-logo").owlCarousel({
-		autoPlay: 3000,
+		autoPlay: 10000,
 		items : 5,
 		itemsDesktop : [1199,5],
 		itemsDesktopSmall : [979,5],
